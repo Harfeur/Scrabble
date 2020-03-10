14ffb591a967a60058e33718a3a1af93937ec90b
Llaplace <lisa.laplace12@gmail.com>
2020-03-10 13:45:23 +0100

    Nouvelle Structure : Chevalet (incomplet)

---
+++ src/fr/scrabble/structures/Chevalet.java
@@ -0,0 +1,16 @@
+package fr.scrabble.structures;
+
+public class Chevalet {
+
+	Lettre chevalet[];
+	int nb_lettre=0;
+	
+	public Chevalet() {
+		this.chevalet = new Lettre[7];
+		this.nb_lettre=0;
+	}
+	
+	public void remplir() {
+		
+	}
+}
