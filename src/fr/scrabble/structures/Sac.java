e13215fd9bde6cf8216fe7a80171330debc7a23f
Harfeur <maxou3153@gmail.com>
2020-03-10 13:48:42 +0100

    Nouvelle structure : Sac

---
+++ src/fr/scrabble/structures/Sac.java
@@ -0,0 +1,22 @@
+package fr.scrabble.structures;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+@SuppressWarnings("serial")
+public class Sac extends ArrayList<Lettre> {
+	
+	public Sac() {
+		super();
+	}
+	
+	public void ajouterLettre(Lettre lettre) {
+		this.add(lettre);
+	}
+	
+	public Lettre obtenirLettre() {
+		Random r = new Random();
+		return this.remove(r.nextInt(this.size()));
+	}
+	
+}
