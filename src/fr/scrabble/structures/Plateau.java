2bfe1d9df92b0dd663dc12c16716b683a0063550
Llaplace <lisa.laplace12@gmail.com>
2020-03-11 14:59:38 +0100

    Réparation plateau

--- src/fr/scrabble/structures/Plateau.java
+++ src/fr/scrabble/structures/Plateau.java
@@ -8,40 +8,46 @@ import java.io.IOException;
 import fr.scrabble.structures.Case.Multiplicateur;
 
 public class Plateau {
-	
+
 	Case[][] plateau;
-	
+
 	public Plateau() {
 		super();
-		this.plateau = new Case[16][16];
+		this.plateau = new Case[15][15];
 		File fichier=new File("assets/plateau.csv");
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(fichier));
 			String strCurrentLine;
 			int ligne = 0;
-		    while ((strCurrentLine = reader.readLine()) != null) {
-		    	String[] tab = strCurrentLine.split(",");
-		    	for (int col = 0; col < tab.length; col++) {
-		    		Multiplicateur m;
-		    		switch (tab[col]) {
-						case "2W": m = Multiplicateur.MOT_DOUBLE; break;
-						case "3W": m = Multiplicateur.MOT_TRIPLE; break;
-						case "2L": m = Multiplicateur.LETTRE_DOUBLE; break;
-						case "3L": m = Multiplicateur.LETTRE_TRIPLE; break;
-						default: m = Multiplicateur.SIMPLE;
+			while ((strCurrentLine = reader.readLine()) != null) {
+				String[] tab = strCurrentLine.split(",");
+				System.out.println(tab.length);
+				for (int col = 0; col < 15; col++) {
+					Multiplicateur m;
+					if (tab.length > col) {
+						switch (tab[col]) {
+							case "2W": m = Multiplicateur.MOT_DOUBLE; break;
+							case "3W": m = Multiplicateur.MOT_TRIPLE; break;
+							case "2L": m = Multiplicateur.LETTRE_DOUBLE; break;
+							case "3L": m = Multiplicateur.LETTRE_TRIPLE; break;
+							default: m = Multiplicateur.SIMPLE;
+						}
+						this.plateau[ligne][col] = new Case(m);
+					} else {
+						this.plateau[ligne][col] = new Case(Multiplicateur.SIMPLE);
 					}
-					this.plateau[ligne][col] = new Case(m);
 				}
-		    	ligne++;
-		    }
+				ligne++;
+			}
 		} catch(IOException e1) {
 			System.out.print("Erreur");
 			System.exit(0);
 		}
+		System.out.println();
 	}
-	
+
 	public Case getCase(int ligne, int colonne) {
 		return this.plateau[ligne][colonne];
 	}
-	
+
 }
