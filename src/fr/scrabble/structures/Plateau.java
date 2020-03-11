ef4bf6eadd64c5eb582c449b7b0e762ff6d617e5
Harfeur <maxou3153@gmail.com>
2020-03-11 13:49:54 +0100

    Création Plateau

--- src/fr/scrabble/structures/Plateau.java
+++ src/fr/scrabble/structures/Plateau.java
@@ -1,5 +1,14 @@
 package fr.scrabble.structures;
 
+import fr.scrabble.structures.Case.Multiplicateur;
+
 public class Plateau {
+	
+	Case[][] plateau;
+	
+	public Plateau() {
+		super();
+		this.plateau = new Case[16][16];
+	}
 	
 }
