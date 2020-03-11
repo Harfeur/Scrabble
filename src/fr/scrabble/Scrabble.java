3a98eb98e33ca9d5c4d624809a974b97197aac6e
Llaplace <lisa.laplace12@gmail.com>
2020-03-11 14:15:34 +0100

    Ajout echelle

--- src/fr/scrabble/Scrabble.java
+++ src/fr/scrabble/Scrabble.java
@@ -2,6 +2,8 @@ package fr.scrabble;
 
 public class Scrabble {
 
+	public static double SCALE=1.0; 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Modele m=new Modele();
