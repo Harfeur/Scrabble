b2834ea85b050c1d0fc661b3a353ae748563d718
Harfeur <maxou3153@gmail.com>
2020-03-11 13:47:18 +0100

    Suppression position

--- src/fr/scrabble/structures/Case.java
+++ src/fr/scrabble/structures/Case.java
@@ -1,15 +1,11 @@
 package fr.scrabble.structures;
 
 public class Case {
-	enum Multiplicateur {LETTRE_DOUBLE,MOT_DOUBLE,LETTRE_TRIPLE,MOT_TRIPLE}; 
-	char ligne;
-	int colonne;
+	enum Multiplicateur {SIMPLE,LETTRE_DOUBLE,MOT_DOUBLE,LETTRE_TRIPLE,MOT_TRIPLE};
 	Multiplicateur multiplicateur;
 	Lettre lettre;
 	
-	public Case(char lig, int col, Multiplicateur multi) {
-		this.ligne=lig;
-		this.colonne=col;
+	public Case(Multiplicateur multi) {
 		this.multiplicateur=multi;
 	}
 	
