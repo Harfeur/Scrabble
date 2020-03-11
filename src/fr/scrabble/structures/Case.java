54940a7381a702b948572361d9b50aad286b1754
Llaplace <lisa.laplace12@gmail.com>
2020-03-11 14:59:59 +0100

    Modif enum

--- src/fr/scrabble/structures/Case.java
+++ src/fr/scrabble/structures/Case.java
@@ -1,7 +1,29 @@
 package fr.scrabble.structures;
 
+import java.awt.Color;
+
 public class Case {
-	public enum Multiplicateur {SIMPLE,LETTRE_DOUBLE,MOT_DOUBLE,LETTRE_TRIPLE,MOT_TRIPLE};
+	public enum Multiplicateur {
+		
+		SIMPLE (Color.green, "green"),
+		LETTRE_DOUBLE (Color.cyan, "cyan"),
+		MOT_DOUBLE (Color.pink, "pink"),
+		LETTRE_TRIPLE (Color.blue, "blue"),
+		MOT_TRIPLE (Color.red, "red");
+		
+		private Color couleur;
+		private String libelle;
+		
+		private Multiplicateur(Color c, String l) {
+			this.couleur = c;
+			this.libelle = l;
+		}
+		
+		public String toString() { return this.libelle; }
+		public Color getCouleur() { return this.couleur; }
+	};
+	
+	
 	public Multiplicateur multiplicateur;
 	public Lettre lettre;
 	
@@ -13,4 +35,5 @@ public class Case {
 		this.lettre =lettre;
 		// TODO: Exception
 	}
+	
 }
