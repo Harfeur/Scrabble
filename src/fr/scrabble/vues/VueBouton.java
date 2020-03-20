2ad9534832fc40e6aadf99e06b13d8dca1df2bf0
Llaplace <lisa.laplace12@gmail.com>
2020-03-20 18:21:08 +0100

    Mise à jour : VueBouton

--- src/fr/scrabble/vues/VueBouton.java
+++ src/fr/scrabble/vues/VueBouton.java
@@ -1,7 +1,16 @@
 package fr.scrabble.vues;
 
+import java.awt.Button;
 import java.awt.Frame;
+
+import fr.scrabble.controleurs.ControleurBouton;
 
 public class VueBouton extends Frame {
 
+	public VueBouton(ControleurBouton cb) {
+		super();
+		this.add(new Button("Valider"));
+		
+	}
+	
 }
