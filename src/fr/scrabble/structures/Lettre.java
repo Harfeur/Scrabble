5f5f2fb7ffdbf8886981d2666653c31f539a2eb1
Harfeur <maxou3153@gmail.com>
2020-03-22 11:52:10 +0100

    Tests Lettre

--- src/fr/scrabble/structures/Lettre.java
+++ src/fr/scrabble/structures/Lettre.java
@@ -3,6 +3,7 @@ package fr.scrabble.structures;
 import java.io.Serializable;
 
 public class Lettre implements Serializable {
+	
 	private static final long serialVersionUID=1L;
 	public String lettre;
 	public int valeur;
