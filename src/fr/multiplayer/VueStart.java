562a89c8fc0b95338975294b31b43c371b20b869
Llaplace <lisa.laplace12@gmail.com>
2020-04-02 17:45:35 +0200

    Ajout label/texte prenom

--- src/fr/multiplayer/VueStart.java
+++ src/fr/multiplayer/VueStart.java
@@ -1,20 +1,30 @@
 package fr.multiplayer;
 
-import java.awt.Frame;
-import java.awt.Panel;
+import javax.swing.JFormattedTextField;
+import javax.swing.JFrame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.text.MaskFormatter;
+
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
+
 @SuppressWarnings("serial")
-public class VueStart extends Panel implements ActionListener {
+public class VueStart extends JPanel implements ActionListener {
 	
 	Client client;
-	Frame f;
+	JFrame f;
 
 	public VueStart(Client client) {
 		super();
 		this.client = client;
 		//TODO Afficher deux zones de texte (prénom, ip) et un bouton (Valider)
+		JLabel prenom_l = new JLabel("Prénom");
+		JTextField prenom_t = new JTextField("Bernadette");
+		this.add(prenom_l);
 	}
 
 	@Override
