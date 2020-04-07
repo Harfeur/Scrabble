6e6d006dc1a4cab2fc16a0078066a8564bf0cefc
Llaplace <lisa.laplace12@gmail.com>
2020-04-07 19:17:42 +0200

    Mise à jour de l'échelle pour JTextField
    //Suppression import inutile

--- src/fr/scrabble/multiplayer/vues/VueStart.java
+++ src/fr/scrabble/multiplayer/vues/VueStart.java
@@ -1,10 +1,7 @@
 package fr.scrabble.multiplayer.vues;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
-
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.event.*;
 
 import javax.swing.JLabel;
@@ -12,9 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import fr.scrabble.menu.Menu;
-import fr.scrabble.menu.vues.VueMenu;
 import fr.scrabble.multiplayer.Client;
-import javafx.scene.paint.Color;
 
 @SuppressWarnings("serial")
 public class VueStart extends JPanel implements ActionListener {
@@ -26,15 +21,21 @@ public class VueStart extends JPanel implements ActionListener {
 		super();
 		this.client = client;
 		
-		Font font = new Font("Arial",Font.BOLD,(int) (25*Menu.SCALE));
-		JLabel prenom_l = new JLabel("Prénom :");
-		prenom_l.setFont(font);
+		Font font = new Font("Arial",Font.BOLD,(int) (20*Menu.SCALE));
 		
-		this.prenom_t = new JTextField("Bernadette",10);
-		JLabel ip_l = new JLabel("Adresse IP :");
-		ip_l.setFont(font);
+		JLabel prenom_l = new JLabel("Prénom : ");
+		JLabel ip_l = new JLabel("Adresse IP : ");	
+		
+		this.prenom_t = new JTextField("Bernadette",10);	
 		this.ip_t = new JTextField("127.0.0.1",10);
+		
 		JButton valider = new JButton("Valider");
+		
+		prenom_l.setFont(font);
+		this.prenom_t.setFont(font);
+		ip_l.setFont(font);	
+		this.ip_t.setFont(font);
+		valider.setFont(font);
 		
 		valider.addActionListener(this);
 		
