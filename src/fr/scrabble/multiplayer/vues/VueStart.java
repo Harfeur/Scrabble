05fc59d2154f377f8c671d5b801d4bc8e55d55ec
Llaplace <lisa.laplace12@gmail.com>
2020-04-07 17:24:33 +0200

    Mise à jour VueStart

--- src/fr/scrabble/multiplayer/vues/VueStart.java
+++ src/fr/scrabble/multiplayer/vues/VueStart.java
@@ -3,34 +3,38 @@ package fr.scrabble.multiplayer.vues;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
+import java.awt.GridLayout;
 import java.awt.event.*;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import fr.scrabble.menu.vues.VueMenu;
 import fr.scrabble.multiplayer.Client;
+import javafx.scene.paint.Color;
 
 @SuppressWarnings("serial")
 public class VueStart extends JPanel implements ActionListener {
 	
 	Client client;
-	JFrame f;
 	JTextField prenom_t,ip_t;
+	VueMenu vueMenu;
+	JFrame f;
 
 	public VueStart(Client client) {
 		super();
 		this.client = client;
-		f = new JFrame("Start"); 
+		vueMenu = new VueMenu();
 		
-		JLabel prenom_l = new JLabel("Prénom");
-		this.prenom_t = new JTextField("Bernadette");
-		JLabel ip_l = new JLabel("Adresse IP");
-		this.ip_t = new JTextField("127.0.0.1");
+		JLabel prenom_l = new JLabel("Prénom :");
+		this.prenom_t = new JTextField("Bernadette",10);
+		JLabel ip_l = new JLabel("Adresse IP :");
+		this.ip_t = new JTextField("127.0.0.1",10);
 		JButton valider = new JButton("Valider");
 		
 		valider.addActionListener(this);
-			
+		
 		this.add(prenom_l);
 		this.add(prenom_t);
 		this.add(ip_l);
@@ -41,11 +45,9 @@ public class VueStart extends JPanel implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		//TODO Puis fermer la fenetre
-		
 		String prenom = this.prenom_t.getText();
 		String ip = this.ip_t.getText();
-		client.demarrer(ip, prenom);
+		client.demarrer(ip, prenom);		
 	}
 	
 	
