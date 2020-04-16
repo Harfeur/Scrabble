8d82c993bfd29815adfb63c03870601c9ee2699d
Llaplace <lisa.laplace12@gmail.com>
2020-04-16 16:27:55 +0200

    MAJ VueConsole
    Deplacement de l'emplacement

--- src/fr/scrabble/online/vues/VueConsole.java
+++ src/fr/scrabble/online/vues/VueConsole.java
@@ -6,6 +6,7 @@ import java.io.PrintStream;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import fr.scrabble.game.vues.VueColonne;
@@ -20,13 +21,14 @@ public class VueConsole extends JPanel {
 	public VueConsole () {
 		super();
 		txt = new JTextArea(20,20);
+		JScrollPane scrollPane = new JScrollPane (txt);
 		txt.setEditable(false);
 		PrintStream print = new PrintStream(new CustomOutputStream(txt));
 		System.setOut(print);
 		System.setErr(print);
 
 		this.setPreferredSize(new Dimension((int) (VuePlateau.TAILLE*Menu.SCALE),(int) (VuePlateau.TAILLE*10*Menu.SCALE)));
-		this.setBounds((int) (VueColonne.TAILLE*Menu.SCALE+VuePlateau.TAILLE*15*Menu.SCALE)+5,200, (int) (VuePlateau.TAILLE*7*Menu.SCALE),(int) (VuePlateau.TAILLE*10*Menu.SCALE));
+		this.setBounds((int) (VueColonne.TAILLE*Menu.SCALE+VuePlateau.TAILLE*15*Menu.SCALE),(int) (200*Menu.SCALE), (int) (VuePlateau.TAILLE*7*Menu.SCALE),(int) (VuePlateau.TAILLE*10*Menu.SCALE));
 		this.add(txt);
 	}
 	
