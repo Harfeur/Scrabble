e754fe3eb5d83088303be55be052591ccbbf1247
Llaplace <lisa.laplace12@gmail.com>
2020-04-27 20:30:44 +0200

    MAJ VueJoker

--- src/fr/scrabble/game/vues/VueJoker.java
+++ src/fr/scrabble/game/vues/VueJoker.java
@@ -1,5 +1,6 @@
 package fr.scrabble.game.vues;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
+import java.io.IOException;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -20,37 +22,33 @@ public class VueJoker extends JFrame implements ListSelectionListener {
 	
 	JList<String> li;
 	Modele modele;
+	ArrayList<String> lettre ;
 	
 	public VueJoker(String langue, Modele m) {
 		super("Choix de la valeur");
-		this.setPreferredSize(new Dimension((int) (100*Menu.SCALE),(int) (200*Menu.SCALE)));
 		this.modele = m;
 		
 		JPanel panel = new JPanel();
 		
 		Sac sac = new Sac(langue);
 		
-    	ArrayList<Image> lettre = new ArrayList<Image>();
+    	this.lettre = new ArrayList<String>();
 		
 		for(int i=0;i<sac.size();i++) 
         {
             if (!sac.get(i).lettre.equals("JOKER") && !lettre.contains(sac.get(i).lettre)) {
-            	//try {
-        			//lettre.add(ImageIO.read(Menu.class.getResource("/resources/images/letter_"+sac.get(i).lettre+".jpg")));
-        		//} catch (IOException e1) {
-//        			e1.printStackTrace();
-  //      		}	
+            	lettre.add(sac.get(i).lettre);
             }          
         }
 		
 		li = new JList<String>(lettre.toArray(new String[lettre.size()]));
-	    JScrollPane liScroll = new JScrollPane(li); //marche pas 
-		
+	    
 	    ListSelectionModel listSelectionModel = li.getSelectionModel();
 	    listSelectionModel.addListSelectionListener(this);
 
+	    li.setCellRenderer(new JokerList());
 	    li.setLayoutOrientation(JList.VERTICAL_WRAP);
-	    li.setVisibleRowCount(3);
+	    li.setVisibleRowCount(2);
 		
 	    
 		panel.add(li);
@@ -64,13 +62,37 @@ public class VueJoker extends JFrame implements ListSelectionListener {
 	    
 	    this.setLocation(x, y);
 		this.setVisible(true);
+
 	}
+
+
 
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
 		String lettre = li.getSelectedValue();
+		System.out.println(lettre);
 		this.modele.lettreJoker(lettre);
 		this.setVisible(false);
 		this.dispose();
 	}
+
+	
+	class JokerList extends DefaultListCellRenderer{
+		
+		public Component getListCellRendererComponent(JList list, Object value, int index,boolean isSelected, boolean cellHasFocus) {
+			JLabel label = (JLabel) super.getListCellRendererComponent(
+                    list, value, index, isSelected, cellHasFocus);
+			for(int i=0;i<lettre.size();i++) {
+	        	if(value.equals(lettre.get(i))) {
+	        		Image im = null;
+					im = new ImageIcon(getClass().getResource("/resources/images/lettre/letter_"+lettre.get(i)+".png")).getImage().getScaledInstance((int)(VuePlateau.TAILLE*Menu.SCALE), (int) (VuePlateau.TAILLE*Menu.SCALE), Image.SCALE_DEFAULT);
+	        		label.setIcon(new ImageIcon(im));
+	        		label.setText("");
+	        	}
+	        }
+            return label;
+		}			
+	}	
+	
 }
+
