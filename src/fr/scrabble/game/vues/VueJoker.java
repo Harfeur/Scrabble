fd789fcedd3b93537350cd21b4934678da7b48c2
Llaplace <lisa.laplace12@gmail.com>
2020-04-27 18:01:12 +0200

    MAJ vueJoker

--- src/fr/scrabble/game/vues/VueJoker.java
+++ src/fr/scrabble/game/vues/VueJoker.java
@@ -1,14 +1,18 @@
 package fr.scrabble.game.vues;
 
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Toolkit;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import fr.scrabble.game.Modele;
+import fr.scrabble.menu.Menu;
 import fr.scrabble.structures.Sac;
 
 @SuppressWarnings("serial")
@@ -19,26 +23,36 @@ public class VueJoker extends JFrame implements ListSelectionListener {
 	
 	public VueJoker(String langue, Modele m) {
 		super("Choix de la valeur");
-		
+		this.setPreferredSize(new Dimension((int) (100*Menu.SCALE),(int) (200*Menu.SCALE)));
 		this.modele = m;
 		
 		JPanel panel = new JPanel();
 		
 		Sac sac = new Sac(langue);
 		
-    	ArrayList<String> strings = new ArrayList<String>();
+    	ArrayList<Image> lettre = new ArrayList<Image>();
 		
 		for(int i=0;i<sac.size();i++) 
         {
-            if (!sac.get(i).lettre.equals("JOKER") && !strings.contains(sac.get(i).lettre)) {
-            	strings.add(sac.get(i).lettre);
+            if (!sac.get(i).lettre.equals("JOKER") && !lettre.contains(sac.get(i).lettre)) {
+            	//try {
+        			//lettre.add(ImageIO.read(Menu.class.getResource("/resources/images/letter_"+sac.get(i).lettre+".jpg")));
+        		//} catch (IOException e1) {
+//        			e1.printStackTrace();
+  //      		}	
             }          
         }
 		
-		li = new JList<String>(strings.toArray(new String[strings.size()]));
-		ListSelectionModel listSelectionModel = li.getSelectionModel();
-	    listSelectionModel.addListSelectionListener(this);
+		li = new JList<String>(lettre.toArray(new String[lettre.size()]));
+	    JScrollPane liScroll = new JScrollPane(li); //marche pas 
 		
+	    ListSelectionModel listSelectionModel = li.getSelectionModel();
+	    listSelectionModel.addListSelectionListener(this);
+
+	    li.setLayoutOrientation(JList.VERTICAL_WRAP);
+	    li.setVisibleRowCount(3);
+		
+	    
 		panel.add(li);
 		this.add(panel);
 	
