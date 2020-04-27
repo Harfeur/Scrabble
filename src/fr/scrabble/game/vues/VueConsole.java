f4e11f56ed00ee5005f5a8cfaa072f858112402f
Llaplace <lisa.laplace12@gmail.com>
2020-04-27 16:01:34 +0200

    MAJ VueConsole

--- src/fr/scrabble/game/vues/VueConsole.java
+++ src/fr/scrabble/game/vues/VueConsole.java
@@ -2,26 +2,34 @@ package fr.scrabble.game.vues;
 
 import java.awt.Dimension;
 import java.io.PrintStream;
+import java.util.Observable;
+import java.util.Observer;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import fr.scrabble.game.Modele;
 import fr.scrabble.game.utils.CustomOutputStream;
 import fr.scrabble.menu.Menu;
+import fr.scrabble.structures.Plateau;
 
 @SuppressWarnings("serial")
-public class VueConsole extends JPanel {
+public class VueConsole extends JPanel implements Observer {
 
-	JTextArea txt;
+	JTextArea JtxtA;
+	Modele modele;
+	String txt;
 	
-	public VueConsole () {
+	public VueConsole (Modele m) {
 		super();
-		txt = new JTextArea(20,20);
-		JScrollPane scrollPane = new JScrollPane(txt);
-		txt.setEditable(false);
+		this.modele = m;
+		
+		JtxtA = new JTextArea(20,20);
+		JScrollPane scrollPane = new JScrollPane(JtxtA);
+		JtxtA.setEditable(false);
 
-		PrintStream print = new PrintStream(new CustomOutputStream(txt));
+		PrintStream print = new PrintStream(new CustomOutputStream(JtxtA));
 		System.setOut(print);
 		System.setErr(print);
 
@@ -29,5 +37,13 @@ public class VueConsole extends JPanel {
 		this.setBounds((int) (VueColonne.TAILLE*Menu.SCALE+VuePlateau.TAILLE*15*Menu.SCALE),(int) (200*Menu.SCALE), (int) (VuePlateau.TAILLE*7*Menu.SCALE),(int) (VuePlateau.TAILLE*10*Menu.SCALE));
 		this.add(scrollPane);
 	}
-	
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		if(arg.getClass() == String.class) {
+			this.txt = (String) arg;
+			this.JtxtA.append(this.txt);
+		}
+	}
 }
--- src/fr/scrabble/menu/Menu.java
+++ src/fr/scrabble/menu/Menu.java
@@ -116,11 +116,8 @@ public class Menu extends JFrame implements Observer {
 		this.vueScore = new VueScore(this);
 		loading.setValue(7);
 		
-		this.vueConsole = new VueConsole();
-		loading.setValue(8);
-		
 		this.vueRejete = new VueRejete(this.couleur);
-		loading.setValue(9);
+		loading.setValue(8);
 
 		this.vueMenu();
 	}
@@ -185,9 +182,11 @@ public class Menu extends JFrame implements Observer {
 		VuePlateau vuePlateau = new VuePlateau(cp,this);
 		VueChevalet vueChevalet = new VueChevalet(cc, this);
 		VueBouton vueBouton = new VueBouton(cb);
+		VueConsole vueConsole = new VueConsole(modeleHorsLigne);
 
 		this.modeleHorsLigne.addObserver(vuePlateau);
 		this.modeleHorsLigne.addObserver(vueChevalet);
+		this.modeleHorsLigne.addObserver(vueConsole);
 		this.modeleHorsLigne.addObserver(vueScore);
 		this.modeleHorsLigne.addObserver(this);
 
@@ -224,9 +223,11 @@ public class Menu extends JFrame implements Observer {
 		VuePlateau vuePlateau = new VuePlateau(cp,this);
 		VueChevalet vueChevalet = new VueChevalet(cc, this);
 		VueBouton vueBouton = new VueBouton(cb);
+		VueConsole vueConsole = new VueConsole(modeleHorsLigne);
 
 		this.modeleHorsLigne.addObserver(vuePlateau);
 		this.modeleHorsLigne.addObserver(vueChevalet);
+		this.modeleHorsLigne.addObserver(vueConsole);
 		this.modeleHorsLigne.addObserver(vueScore);
 		this.modeleHorsLigne.addObserver(this);
 
