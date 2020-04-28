586d9bee0cbd19c072eefa3a86c07a2c1bab6c7c
Harfeur <maxou3153@gmail.com>
2020-04-28 22:13:23 +0200

    Chargement plus rapide

--- src/fr/scrabble/game/vues/VuePlateau.java
+++ src/fr/scrabble/game/vues/VuePlateau.java
@@ -1,16 +1,15 @@
 package fr.scrabble.game.vues;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.MouseInputListener;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.io.IOException;
+import java.awt.MediaTracker;
+import java.awt.Toolkit;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -30,8 +29,9 @@ public class VuePlateau extends JPanel implements Observer {
 	Couleur c;
 	//Color [clair fill,sombre fill]
 	Color[] tuile = {new Color(230,207,207),new Color(200,77,77)};
+	ArrayList<Image> images;
 
-	
+
 	public VuePlateau(MouseInputListener l, Menu menu) {
 		super();
 		this.setPreferredSize(new Dimension((int) (VuePlateau.TAILLE*15*Menu.SCALE),(int) (VuePlateau.TAILLE*15*Menu.SCALE)));
@@ -40,14 +40,50 @@ public class VuePlateau extends JPanel implements Observer {
 		this.menu = menu;
 		this.c = menu.couleur;
 
+		// Chargement des images
+		this.images = new ArrayList<Image>();
+		MediaTracker mt = new MediaTracker(this);
+
+		for (int i = 0; i < 27; i++) {
+			String lettre;
+			if (i == 26) {
+				lettre = "JOKER";
+			} else {
+				Character c = (char) ('A'+i);
+				lettre = c.toString();
+			}
+			Image img = Toolkit.getDefaultToolkit().getImage(Lettre.class.getResource("/resources/images/lettre/letter_"+lettre+".png"));
+			mt.addImage(img, i);
+			this.images.add(img);
+		}
+
+		this.images.add(Toolkit.getDefaultToolkit().getImage(Multiplicateur.class.getResource("/resources/images/plateau/E.png")));
+		mt.addImage(this.images.get(this.images.size()-1), 27);
+		this.images.add(Toolkit.getDefaultToolkit().getImage(Multiplicateur.class.getResource("/resources/images/plateau/LD.png")));
+		mt.addImage(this.images.get(this.images.size()-1), 28);
+		this.images.add(Toolkit.getDefaultToolkit().getImage(Multiplicateur.class.getResource("/resources/images/plateau/LT.png")));
+		mt.addImage(this.images.get(this.images.size()-1), 29);
+		this.images.add(Toolkit.getDefaultToolkit().getImage(Multiplicateur.class.getResource("/resources/images/plateau/MD.png")));
+		mt.addImage(this.images.get(this.images.size()-1), 30);
+		this.images.add(Toolkit.getDefaultToolkit().getImage(Multiplicateur.class.getResource("/resources/images/plateau/MT.png")));
+		mt.addImage(this.images.get(this.images.size()-1), 31);
+		this.images.add(Toolkit.getDefaultToolkit().getImage(Multiplicateur.class.getResource("/resources/images/plateau/S.png")));
+		mt.addImage(this.images.get(this.images.size()-1), 32);
+
+		try {
+			mt.waitForAll();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
 		// Creation du Panel
 		VueColonne colonne = new VueColonne(this.menu);
 		VueLigne ligne = new VueLigne(this.menu);
-		
+
 		this.setBackground(Color.GREEN);
-        this.setBounds((int) (colonne.getWidth()), (int) (ligne.getHeight()), (int) (VuePlateau.TAILLE*15*Menu.SCALE), (int) (VuePlateau.TAILLE*15*Menu.SCALE));
+		this.setBounds((int) (colonne.getWidth()), (int) (ligne.getHeight()), (int) (VuePlateau.TAILLE*15*Menu.SCALE), (int) (VuePlateau.TAILLE*15*Menu.SCALE));
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
@@ -55,32 +91,41 @@ public class VuePlateau extends JPanel implements Observer {
 			for (int i = 0; i < 15; i++) {
 				for (int j = 0; j < 15; j++) {
 					Case c = this.plateau.getCase(i,j);	
-					if(c.lettre==null) {				
+					if(c.lettre==null) {	
+						int index = 25;
 						Multiplicateur m = c.multiplicateur;
-						Image im = null;
-						try {
-							if(i==7 && j==7) {
-								im = ImageIO.read(Multiplicateur.class.getResource("/resources/images/plateau/E.png"));
-							}
-							else {
-								im = ImageIO.read(Multiplicateur.class.getResource("/resources/images/plateau/"+m.toString()+".png"));
-							}
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+						switch(m) {				
+						case LETTRE_DOUBLE:
+							index = 28;
+							break;
+						case LETTRE_TRIPLE:
+							index = 29;
+							break;
+						case MOT_DOUBLE:
+							index = 30;
+							break;
+						case MOT_TRIPLE:
+							index = 31;
+							break;
+						case SIMPLE:
+							index = 32;
 						}
-						g.drawImage(im,(int) (j*VuePlateau.TAILLE*Menu.SCALE), (int) (i*VuePlateau.TAILLE*Menu.SCALE),(int) (VuePlateau.TAILLE*Menu.SCALE),(int) (VuePlateau.TAILLE*Menu.SCALE),null);
+						if(i==7 && j==7) {
+							index = 27;
 						}
+						g.drawImage(this.images.get(index),(int) (j*VuePlateau.TAILLE*Menu.SCALE), (int) (i*VuePlateau.TAILLE*Menu.SCALE),(int) (VuePlateau.TAILLE*Menu.SCALE),(int) (VuePlateau.TAILLE*Menu.SCALE),null);
+					}
 					else {
-						Image im = null;
-						try {
-							im = ImageIO.read(Lettre.class.getResource("/resources/images/lettre/letter_"+c.lettre.lettre+".png"));
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+						int index;
+						if (c.lettre.valeur == 0)
+							index = 26;
+						else {
+							char lettre = c.lettre.lettre.charAt(0);
+							index = (int) lettre;
+							index -= 65;
 						}
-						g.drawImage(im,(int) (j*VuePlateau.TAILLE*Menu.SCALE), (int) (i*VuePlateau.TAILLE*Menu.SCALE),(int) (VuePlateau.TAILLE*Menu.SCALE),(int) (VuePlateau.TAILLE*Menu.SCALE),null);
-						}
+						g.drawImage(this.images.get(index),(int) (j*VuePlateau.TAILLE*Menu.SCALE), (int) (i*VuePlateau.TAILLE*Menu.SCALE),(int) (VuePlateau.TAILLE*Menu.SCALE),(int) (VuePlateau.TAILLE*Menu.SCALE),null);
+					}
 				}
 			}
 		}
@@ -103,5 +148,5 @@ public class VuePlateau extends JPanel implements Observer {
 			this.putClientProperty("color", this.c.getCouleur());
 		}
 	}
-	
+
 }
