8afe35dc9d5e8e1a0833a8482fee73e5f1a3f2d7
Harfeur <maxou3153@gmail.com>
2020-05-05 21:24:16 +0200

    Ajout Junit

---
+++ src/fr/scrabble/structures/junit/CaseTest.java
@@ -0,0 +1,19 @@
+package fr.scrabble.structures.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CaseTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
