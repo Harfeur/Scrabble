9fa7cc682176d9f914d192681cb048c82d1ad8eb
Llaplace <lisa.laplace12@gmail.com>
2020-05-05 13:51:56 +0200

    MAJ APropos

--- src/fr/scrabble/menu/vues/APropos.java
+++ src/fr/scrabble/menu/vues/APropos.java
@@ -1,7 +1,10 @@
 package fr.scrabble.menu.vues;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.util.ResourceBundle;
 
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
 
 import fr.scrabble.menu.Menu;
@@ -13,7 +16,21 @@ public class APropos extends JFrame{
 		ResourceBundle strings = ResourceBundle.getBundle("resources/i18n/strings", menu.getLocale());
 		this.setTitle(strings.getString("asavoir"));
 		
+		JEditorPane jText = new JEditorPane("text/html", strings.getString("pourquoi"));
+		
+		this.add(jText);
+		
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		this.setPreferredSize(new Dimension((int) (Menu.SCALE*500), (int) (Menu.SCALE*500)));
+
 		this.pack();
+
+		Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		int x = (int) ((dimension.getWidth() - this.getWidth()) / 2);
+		int y = (int) ((dimension.getHeight() - this.getHeight()) / 2);
+
+		this.setLocation(x, y);
 		this.setVisible(true);
 	}
 }
