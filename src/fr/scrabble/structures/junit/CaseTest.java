f0dd75c57fd226812ea86fab40f9d56bdb508793
Llaplace <lisa.laplace12@gmail.com>
2020-05-06 14:39:28 +0200

    MAJ CaseTest

--- src/fr/scrabble/structures/junit/CaseTest.java
+++ src/fr/scrabble/structures/junit/CaseTest.java
@@ -5,15 +5,28 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import fr.scrabble.structures.Case;
+import fr.scrabble.structures.Case.Multiplicateur;
+import fr.scrabble.structures.Lettre;
+
 class CaseTest {
 
+	Case c;
+	Lettre l;
+	Multiplicateur multi;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		c = new Case(multi);
+		l = new Lettre("A",1);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAjoutLettre() {
+		assertNull(c.lettre);
+		c.ajouterLettre(l);
+		assertEquals(c.lettre.lettre,"A");
+		assertEquals(c.lettre.valeur,1);
 	}
 
 }
